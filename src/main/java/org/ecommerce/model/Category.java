e4e99a072243cfc2cd01d6e2cb982b62d3c780ff
akshay <akshaysj71@gmail.com>
2022-09-04 16:31:41 +0530

    first commit

---
+++ src/main/java/org/ecommerce/model/Category.java
@@ -0,0 +1,43 @@
+package org.ecommerce.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Category {
+
+	public Category() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Category(int categoryId, String categoryName) {
+		super();
+		this.categoryId = categoryId;
+		this.categoryName = categoryName;
+	}
+	@Override
+	public String toString() {
+		return "Category [categoryId=" + categoryId + ", categoryName=" + categoryName + "]";
+	}
+	public int getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+	public String getCategoryName() {
+		return categoryName;
+	}
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	@Id
+	@GeneratedValue
+	private int categoryId;
+	private String categoryName;
+	
+}
